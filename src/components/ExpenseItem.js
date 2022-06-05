dc0b9aca40835120507f0cc6d794e750e3bac4f1
liaisontw <liaison.tw@gmail.com>
2022-06-05 18:25:00 +0800

    Section 35: add css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div className="expense-item">
+            <div>June 5th 2022</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
